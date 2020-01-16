db95694f5cc7e1bbb8b5cc9266c4e66d7a012a42
5L07H <59944797+5L07H@users.noreply.github.com>
2020-01-16 08:22:27 -0500

    Update OpenAI.py

--- OpenAI.py
+++ OpenAI.py
@@ -1,3 +1,12 @@
 import math
 import numpy
-
+global rewards
+rewards = []
+def assignreward(var):
+  global rewards
+  if var is float or int:
+    rewards.append(var)
+   else:
+    raise "inproper type for assignreward()"
+    
+  
